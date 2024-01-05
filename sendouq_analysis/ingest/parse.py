f157cb127a56df3af240811d002557e1239ba1af
Cesar E Garza <cesar@cegarza.com>
2024-01-05 01:54:30 -0600

    Add sendouq_analysis/ingest/__init__.py, sendouq_analysis/ingest/parse.py, and sendouq_analysis/ingest/scrape.py

---
+++ sendouq_analysis/ingest/parse.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+from sendouq_analysis.constants.columns import user_memento as um
+from sendouq_analysis.constants import PREFERENCES
+from sendouq_analysis.utils import camel_to_snake
+
+
+def parse_memento(memento: dict) -> tuple[pd.DataFrame, ...]:
+    """Parses the "memento" field from a match, which contains the match data
+    related to SP. Returns two DataFrames, one for the group data and one for
+    the user data. If "Map Preferences" is part of the memento, a third
+    DataFrame is returned.
+
+    Args:
+        memento (dict): The "memento" field from a match
+
+    Returns:
+        tuple:
+            - pd.DataFrame: The group data
+            - pd.DataFrame: The user data
+            - pd.DataFrame: The map preferences data, if present
+    """
+    groups_data = []
+    for group_id, details in memento["groups"].items():
+        group_info = details.copy()
+        group_info["groupId"] = group_id
+        groups_data.append(group_info)
+
+    groups_df = pd.json_normalize(groups_data, sep="_").rename(
+        columns=camel_to_snake
+    )
+
+    # Parse users
+    users_data = []
+    for user_id, details in memento["users"].items():
+        user_info = details.copy()
+        user_info["userId"] = user_id
+        users_data.append(user_info)
+
+    users_df = pd.json_normalize(users_data, sep="_").rename(
+        columns=camel_to_snake
+    )
+    users_df[um.USER_ID] = users_df[um.USER_ID].astype(int)
+
+    if "map_preferences" not in memento:
+        return groups_df, users_df
+
+    # Parse map preferences
+    map_data = []
+    for map_id, details in enumerate(memento["mapPreferences"]):
+        map_info = pd.json_normalize(details).rename(columns=camel_to_snake)
+        map_info["map_id"] = map_id
+        map_info[um.USER_ID] = map_info[um.USER_ID].astype(int)
+        map_data.append(map_info.fillna(PREFERENCES.EXPLICIT_INDIFFERENT))
+    
+    map_df = pd.concat(map_data)
+    map_df[um.USER_ID] = map_df[um.USER_ID].astype(int)
+
+    return groups_df, users_df, map_df
---
+++ sendouq_analysis/ingest/scrape.py
@@ -0,0 +1,163 @@
+from __future__ import annotations
+
+import json
+import logging
+import os
+import time
+from typing import TYPE_CHECKING
+
+import requests
+
+if TYPE_CHECKING:
+    from typing import Literal
+
+base_url = "https://sendou.ink/q/match/"
+url_suffix = r"?_data=features%2Fsendouq%2Froutes%2Fq.match.%24id"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+
+def scrape_match(match_id: int) -> dict:
+    """Scrapes a single match from sendou.ink
+
+    Args:
+        match_id (int): The id of the match to scrape
+
+    Returns:
+        dict: The match data as a dictionary
+    """
+    logger.info("Scraping match id: %s", match_id)
+    url = base_url + str(match_id) + url_suffix
+    response = requests.get(url)
+    return response.json()
+
+
+def scrape_matches(
+    start_id: int,
+    end_id: int | Literal[False] = False,
+    debounce_amt: float = 0.0,
+) -> list[dict]:
+    """Scrapes a range of matches from sendou.ink
+
+    Args:
+        start_id (int): The id of the first match to scrape
+        end_id (int | Literal[False]): The id of the last match to scrape. If
+            False, scrape all matches from start_id until it errors out.
+        debounce_amt (float): The amount of time to wait between requests
+
+    Returns:
+        list[dict]: A list of match data dictionaries
+    """
+    matches = []
+    if not end_id or end_id < start_id:
+        logger.info("Scraping all matches from id: %s", start_id)
+        end_id = int(1e12)  # 1 trillion, should hold if IDs remain sequential
+    else:
+        logger.info("Scraping matches from id: %s to %s", start_id, end_id)
+
+    try:
+        for match_id in range(start_id, end_id + 1):
+            match_json = scrape_match(match_id)
+            if match_json["match"]["reportedAt"] is None:
+                logger.info(
+                    "Match in progress, ending scrape. Last match id: %s",
+                    match_id - 1,
+                )
+                break
+            matches.append(match_json)
+            if debounce_amt > 0.0:
+                time.sleep(debounce_amt)
+    except json.JSONDecodeError:
+        logger.warning("All matches scraped, last match id: %s", match_id - 1)
+    return matches
+
+
+def scrape_matches_to_files(
+    start_id: int,
+    save_path: str,
+    end_id: int | Literal[False] = False,
+    major_directory_count: int = 1_000,
+    minor_directory_count: int = 100,
+) -> None:
+    """Scrapes a range of matches from sendou.ink and saves them to files
+
+    Args:
+        start_id (int): The id of the first match to scrape
+        save_path (str): The path to save the matches to
+        end_id (int | Literal[False]): The id of the last match to scrape. If
+            False, scrape all matches from start_id until it errors out.
+        major_directory_count (int): The number of matches to save in each
+            major directory. Defaults to 1,000.
+        minor_directory_count (int): The number of matches to save in each
+            minor directory. Defaults to 100.
+    """
+    matches = scrape_matches(start_id, end_id)
+    create_directory_structure(
+        start_id,
+        start_id + len(matches) - 1,
+        save_path,
+        major_directory_count,
+        minor_directory_count,
+    )
+    for i, match in enumerate(matches):
+        write_match_to_file(match, save_path, i)
+
+
+def create_directory_structure(
+    start_id: int,
+    end_id: int,
+    save_path: str,
+    major_directory_count: int = 1_000,
+    minor_directory_count: int = 100,
+) -> None:
+    """Creates the directory structure for saving matches
+
+    Args:
+        start_id (int): The id of the first match to scrape
+        end_id (int): The id of the last match to scrape
+        save_path (str): The path to save the matches to
+        major_directory_count (int): The number of matches to save in each
+            major directory. Defaults to 1,000.
+        minor_directory_count (int): The number of matches to save in each
+            minor directory. Defaults to 100.
+    """
+    num_matches = end_id - start_id + 1
+    max_major_directory = num_matches // major_directory_count
+    max_minor_directory = (
+        num_matches % major_directory_count
+    ) // minor_directory_count
+
+    for major_directory in range(max_major_directory + 1):
+        if major_directory < max_major_directory:
+            minor_directory_end = major_directory_count // minor_directory_count
+        else:
+            minor_directory_end = max_minor_directory + 1
+
+        for minor_directory in range(minor_directory_end):
+            directory_path = os.path.join(
+                save_path, str(major_directory), str(minor_directory)
+            )
+            os.makedirs(directory_path, exist_ok=True)
+
+
+def write_match_to_file(match: dict, save_path: str, match_id: int) -> None:
+    """Writes a match to a file
+
+    Args:
+        match (dict): The match data to write
+        save_path (str): The path to save the match to
+        match_id (int): The id of the match, used to determine the file path
+    """
+    major_directory = match_id // 1_000
+    minor_directory = match_id // 100
+    base_path = os.path.join(
+        save_path, str(major_directory), str(minor_directory)
+    )
+    path = os.path.join(base_path, f"sendouq_{match_id}.json")
+    with open(path, "w") as f:
+        json.dump(match, f)
