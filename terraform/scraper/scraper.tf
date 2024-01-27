7c17f74039250ab272303ef716fa745631dc348f
Cesar E Garza <cesar@cegarza.com>
2024-01-27 16:20:19 -0600

    Update run_scraper.yml and remove cloud-init.yml

--- terraform/scraper/scraper.tf
+++ terraform/scraper/scraper.tf
@@ -11,7 +11,6 @@ resource "digitalocean_droplet" "sendouq_scraper" {
         data.digitalocean_ssh_key.wsl.id,
         data.digitalocean_ssh_key.github_actions_ed25519.id,
     ]
-    # user_data = file("cloud-init.yml")
 }
 
 resource "digitalocean_database_firewall" "sendouq_scraper" {
